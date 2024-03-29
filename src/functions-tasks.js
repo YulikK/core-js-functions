cf9aba7d63de27e309392d2075eeceded19ca6d8
YulikK <yulikpoch@gmail.com>
2024-03-29 15:04:42 +0100

    feat: add task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -138,8 +138,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (e) {
+        if (i === attempts - 1) {
+          throw e;
+        }
+      }
+    }
+    return null;
+  };
 }
 
 /**
